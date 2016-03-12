094a17493f54e42ad979926c5ec8cdc2cb504e70
Danny Allen <danny@cddnation.com>
2016-03-12 13:44:26 +0000

    Test.

--- src/Payment/Gateways/SecureTrading/SecureTrading.php
+++ src/Payment/Gateways/SecureTrading/SecureTrading.php
@@ -185,7 +185,7 @@ class SecureTrading extends Gateway {
 				$errorCode = $error->code();
 			}
 		}
-		while($errorCode == '20004');
+		while($errorCode == '20004' || $errorCode == '20005');
 
 		//return result
 		return $result;
